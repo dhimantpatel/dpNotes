d4ad02b18df458d05baf86d92081e8a70750832b
unknown <drp4kri@gmail.com>
2015-12-27 01:16:47 -0500

    Added DataModel dpNoteFileNameModel.

---
+++ dpNoteFileNameModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class dpNoteFileNameModel
+{
+
+    private string _mFileName;
+    private string _mPassword;
+
+    public string mFileName
+    {
+        get { return _mFileName; }
+        set
+        {
+            if (!_mFileName.Equals(value))
+                _mFileName = value;
+        }
+    }
+
+    public string mPassword
+    {
+        get { return _mPassword; }
+        set
+        {
+            if (!_mPassword.Equals(value))
+                _mPassword = value;
+        }
+
+    }
+
+    public dpNoteFileNameModel()
+	{
+	}
+}
