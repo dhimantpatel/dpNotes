847ec86fca3010160fb09c135afce8e30ca91f36
unknown <drp4kri@gmail.com>
2015-12-27 01:24:43 -0500

    Removed Model folder, Copied dpNoteFileNameModel into dpNotes.Model project.

---
+++ dpNotes.Model/dpNoteFileNameModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace dpNotes.Model
+{
+    public class dpNoteFileNameModel
+    {
+
+        private string _mFileName;
+        private string _mPassword;
+
+        public string mFileName
+        {
+            get { return _mFileName; }
+            set
+            {
+                if (!_mFileName.Equals(value))
+                    _mFileName = value;
+            }
+        }
+
+        public string mPassword
+        {
+            get { return _mPassword; }
+            set
+            {
+                if (!_mPassword.Equals(value))
+                    _mPassword = value;
+            }
+
+        }
+
+        public dpNoteFileNameModel()
+        {
+        }
+    }
+}
